58e1fb5abe3696ca563cb24041eaec361ddf93a2
¨David <davidl1199@hotmail.com>
2024-07-13 12:39:58 -0300

    feat: criando token para validar se o usuário está autenticado.

---
+++ app/Http/Middleware/ValidateToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+        
+        if (!$user) {
+            return response()->json(['message' => 'Unauthenticated.'], 401);
+        }
+        
+        return $next($request);
+    }
+}
